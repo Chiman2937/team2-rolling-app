ff0884ce07884937aa43c4abf88be3030afc7301
Kim Chiyoung <cyga2933@gmail.com>
2025-06-01 20:43:29 +0900

    test: main.jsx에 ModalProvider 임시 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { ToastProvider } from './contexts/ToastProvider.jsx';
+import { ModalProvider } from './contexts/ModalProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToastProvider>
-      <App />
+      <ModalProvider>
+        <App />
+      </ModalProvider>
     </ToastProvider>
   </StrictMode>,
 );
