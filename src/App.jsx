fff16da469e68fb6953cac627371eb448911de92
Hwigeon <bb7788@naver.com>
2025-06-01 23:54:09 +0900

    fix(vite.config): variables를 자동 주입되지 않도록 하고, index.scss에서 불러오도록 설정하였습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Routes from '@/routes/AppRouter';
+import Style from '@/assets/styles/App.module.scss';
 
 function App() {
   return (
-    <div>
+    <div className={Style['app']}>
       {/*TODO: 헤더 컴포넌트 추가*/}
       <main>
         {/* 라우터에 의해 페이지가 랜더링 */}
