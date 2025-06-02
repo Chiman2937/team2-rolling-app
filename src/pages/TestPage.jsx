e4f29021518ebdcd5cec197994d144ce72f3cee1
Hwigeon <bb7788@naver.com>
2025-06-02 21:59:33 +0900

    feat(TestPage): 라우터 테스트를 위한 임시 페이지입니다. #32

---
+++ src/pages/TestPage.jsx
@@ -0,0 +1,9 @@
+function TestPage() {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <p>This is a test page to verify routing and component rendering.</p>
+    </div>
+  );
+}
+export default TestPage;
