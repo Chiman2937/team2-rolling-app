647a47b37e8dba87ce9a6478e1a3d013d1388acb
summerdev96 <developingsummer96@gmail.com>
2025-06-02 18:33:17 +0900

    fix: App.jsx 원복

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Routes from '@/routes/AppRouter';
 import Style from '@/assets/styles/App.module.scss';
-import Dropdown from './components/Dropdown';
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <main>
         {/* 라우터에 의해 페이지가 랜더링 */}
         <Routes />
-        <Dropdown dropdownItems={['친구']} error />
       </main>
     </div>
   );
