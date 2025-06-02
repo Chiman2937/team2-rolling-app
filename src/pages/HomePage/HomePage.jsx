f2adb67aeb2952adf62d7515986e54852082dafb
sooyeon <sy23lee@gmail.com>
2025-06-02 11:48:59 +0900

    feat: 랜딩페이지 반응형 구축

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -11,37 +11,34 @@ const HomePage = () => {
         <article className={styles['card-container']}>
           <div className={styles['card-context']}>
             <span>Point. 01</span>
-            <h1>
-              누구나 손쉽게, 온라인 <br /> 롤링 페이퍼를 만들 수 있어요
-            </h1>
+            <h1>누구나 손쉽게, 온라인 롤링 페이퍼를 만들 수 있어요</h1>
             <p>로그인 없이 자유롭게 만들어요.</p>
           </div>
-          <img
-            className={styles['card-img']}
-            src={imgPaperLg}
-            srcSet={`${imgPaperLg} 768w, ${imgPaperSm} 375w`}
-            sizes='(min-width: 768px) 720px,(min-width: 375px) 369px'
-            width={720}
-            alt='롤링페이퍼 이미지'
-          ></img>
+          <div className={styles['card-img-wrapper']}>
+            <img
+              className={styles['card-img']}
+              src={imgPaperLg}
+              srcSet={`${imgPaperLg} 768w, ${imgPaperSm} 375w`}
+              sizes='(min-width: 768px) 720px,(min-width: 375px) 620px'
+              alt='롤링페이퍼 이미지'
+            ></img>
+          </div>
         </article>
         <article className={`${styles['card-container']} ${styles['reverse']}`}>
           <div className={styles['card-context']}>
             <span>Point. 02</span>
-            <h1>
-              서로에게 이모지로 감정을
-              <br /> 표현해보세요
-            </h1>
+            <h1>서로에게 이모지로 감정을 표현해보세요</h1>
             <p>롤링 페이퍼에 이모지를 추가할 수 있어요.</p>
           </div>
-          <img
-            className={styles['card-img']}
-            src={imgImojiLg}
-            srcSet={`${imgImojiLg} 768w, ${imgImojiSm} 375w`}
-            sizes='(min-width: 768px) 720px,(min-width: 375px) 355px'
-            width={470}
-            alt='이모지 이미지'
-          ></img>
+          <div className={styles['card-img-wrapper']}>
+            <img
+              className={styles['card-img']}
+              src={imgImojiLg}
+              srcSet={`${imgImojiLg} 768w, ${imgImojiSm} 375w`}
+              sizes='(min-width: 768px) 720px,(min-width: 375px) 500px'
+              alt='이모지 이미지'
+            ></img>
+          </div>
         </article>
       </section>
     </main>
