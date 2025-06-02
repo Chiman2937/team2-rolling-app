93643ee984bbda6a5cc13dbe0372e7bd5d590adc
Hwigeon <bb7788@naver.com>
2025-06-03 05:34:38 +0900

    feat(recipientMessageApi): 롤링페이퍼의 메시지 내용 관련 api 모듈 #39

--- src/apis/recipientsAPI.js
+++ src/apis/recipientsAPI.js
@@ -4,7 +4,7 @@ import httpClient from './httpClient';
 export const TEAM = '2';
 
 /* ------------------------------------------------------------------ */
-/* Recipient(수신자) API – Arrow Function 버전                         */
+/* Recipient(수신자) API                        */
 /* ------------------------------------------------------------------ */
 
 /**
