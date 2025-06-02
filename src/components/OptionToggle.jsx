ca792c35e467706c9f91c019bdc134aba4ee4213
Kim Chiyoung <cyga2933@gmail.com>
2025-06-02 18:06:38 +0900

    feat: OptionToggle 애니메이션 추가

--- src/components/OptionToggle.jsx
+++ src/components/OptionToggle.jsx
@@ -1,39 +1,54 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { Children, cloneElement, createContext, useContext, useEffect, useState } from 'react';
 import styles from './OptionToggle.module.scss';
 
 const OptionToggleContext = createContext();
 
-const OptionToggle = ({ children }) => {
+const OptionToggle = ({ children, onChange }) => {
   const [selected, setSelected] = useState(null);
+  const [indicatorPosition, setIndicatorPosition] = useState(0);
 
   useEffect(() => {
     const firstChild = Array.isArray(children) ? children[0] : children;
     const defaultType = firstChild?.props?.type;
     setSelected(defaultType);
-  }, []);
+    onChange?.(defaultType);
+  }, [children]);
+
+  const childrenAppendIndex = Children.map(children, (child, index) => {
+    return cloneElement(child, { index });
+  });
 
   return (
-    <OptionToggleContext.Provider value={{ selected, setSelected }}>
-      <div className={styles['container']}>{children}</div>
+    <OptionToggleContext.Provider value={{ selected, setSelected, setIndicatorPosition, onChange }}>
+      <div className={styles['container']}>
+        {childrenAppendIndex}
+        <div
+          className={styles['indicator']}
+          style={{ transform: `translateX(${indicatorPosition}px)` }}
+        />
+      </div>
     </OptionToggleContext.Provider>
   );
 };
 
-const OptionButton = ({ label, type }) => {
-  const { selected, setSelected } = useContext(OptionToggleContext);
+const OptionButton = ({ index, label, type }) => {
+  const { selected, setSelected, setIndicatorPosition, onChange } = useContext(OptionToggleContext);
 
   const selectedButtonStyle = selected === type ? 'selected' : '';
 
   const handleButtonClick = () => {
     setSelected(type);
+    setIndicatorPosition(index * 122);
+    onChange?.(type);
   };
 
   return (
-    <button
-      className={`${styles['button']} ${styles[selectedButtonStyle]}`}
-      onClick={handleButtonClick}
-    >
-      {label}
+    <button className={`${styles['button']}`} onClick={handleButtonClick}>
+      <span
+        className={`${styles['button-label']}  ${selected === type ? styles[selectedButtonStyle] : ''}`}
+      >
+        {label}
+      </span>
     </button>
   );
 };
