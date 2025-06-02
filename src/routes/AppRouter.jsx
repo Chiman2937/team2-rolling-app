82b5c08a6eab50fb58d776c50cdb7275e8e816d4
Hwigeon <bb7788@naver.com>
2025-06-03 00:51:15 +0900

    test: 에러 바운더리 테스트 #32

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,26 +2,45 @@
 // 라우터를 조합해서 렌더링하는 컴포넌트
 
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { routes } from './routes';
 import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import NotFoundPage from '@/pages/NotFoundPage.jsx';
+import ErrorPage from '@/pages/ErrorPage.jsx';
 //TODO: 로딩 컴포넌트 추가
 function Loading() {
   return <div>Loading…</div>;
 }
-export default function AppRouter() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <Routes>
-        {/* {라우터 배열에서 경로를 탐색함} */}
-        {routes.map(({ path, element: Page }) => (
-          <Route key={path} path={path} element={<Page />} />
-        ))}
 
-        {/* 매칭되는 경로가 없다면 notFound 페이지로 이동 */}
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
-    </Suspense>
+function ExplodeDemo() {
+  const [explode, setExplode] = React.useState(false);
+
+  if (explode) {
+    throw new Error('💣 버튼으로 강제 오류');
+  }
+
+  return <button onClick={() => setExplode(true)}>💣 Click to explode</button>;
+}
+export default function AppRouter() {
+  const location = useLocation();
+  return (
+    <ErrorBoundary FallbackComponent={ErrorPage} resetKeys={[location.pathname]}>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          {/* 테스트용 컴포넌트 */}
+          <Route path='/explode' element={<ExplodeDemo />} />
+
+          {/* 라우터 배열에서 경로를 탐색함 */}
+
+          {routes.map(({ path, element: Page }) => (
+            <Route key={path} path={path} element={<Page />} />
+          ))}
+
+          {/* 매칭되는 경로가 없다면 notFound 페이지로 이동 */}
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </ErrorBoundary>
   );
 }
