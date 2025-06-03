fa36ea5f4be6e8df3e44b54fb26bf6b12cdd08a9
summerdev96 <developingsummer96@gmail.com>
2025-06-03 21:07:56 +0900

    fix: 롤링페이퍼 상세 페이지 반응형 수정

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,10 +1,9 @@
 import styles from '@/components/ItemCard.module.scss';
-import defaultImg from '@/assets/images/image_profile_default.svg';
-import Badge from '@/components/Badge.jsx';
 import DeleteIcon from './DeleteIcon';
 import plusImg from '@/assets/icons/icon_plus.svg';
+import SenderProfile from './SenderProfile';
 
-const ItemCard = ({ cardData, isAddCard }) => {
+const ItemCard = ({ cardData, isAddCard, isEditMode }) => {
   if (isAddCard)
     return (
       <article className={styles['card']}>
@@ -15,29 +14,19 @@ const ItemCard = ({ cardData, isAddCard }) => {
     );
 
   /* 폰트 확인 후 해당 폰트로 보여줘야 함 */
-  const { sender, profileImageURL, relationship, content, createAt } = cardData;
+  const { sender, profileImageURL, content, createAt } = cardData;
 
   return (
     <article className={styles['card']}>
       <div className={styles['card__container']}>
         <div className={styles['card__main']}>
-          <header className={styles['card__author']}>
-            <div className={styles['card__author-content']}>
-              <img
-                src={profileImageURL ? profileImageURL : defaultImg}
-                className={styles['card__author-image']}
-                alt='프로필 이미지'
-              />
-              <div className={styles['card__author-info']}>
-                <div className={styles['card__author-text']}>
-                  From. <div className={styles['card__author-name']}>{sender}</div>
-                </div>
-                <Badge relationType={relationship} />
-              </div>
-            </div>
-            <button className={styles['card__button--delete']}>
-              <DeleteIcon />
-            </button>
+          <header className={styles['card__sender']}>
+            <SenderProfile sender={sender} imageUrl={profileImageURL} />
+            {isEditMode && (
+              <button className={styles['card__button--delete']}>
+                <DeleteIcon />
+              </button>
+            )}
           </header>
           <hr className={styles['card__divider']} />
           <section className={styles['card__content']}>{content}</section>
