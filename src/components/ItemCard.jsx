772c5b831e73d832c3382d4e7791a9e236e8c22e
summerdev96 <developingsummer96@gmail.com>
2025-06-03 18:46:22 +0900

    feat: 카드 컴포넌트 작업, 롤링 상세페이지 작업

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,32 +1,51 @@
 import styles from '@/components/ItemCard.module.scss';
-import defaultImg from '@/assets/images/default_img_profile.png';
+import defaultImg from '@/assets/images/image_profile_default.svg';
 import Badge from '@/components/Badge.jsx';
+import DeleteIcon from './DeleteIcon';
+import plusImg from '@/assets/icons/icon_plus.svg';
 
-function ItemCard() {
+const ItemCard = ({ cardData, isAddCard }) => {
+  if (isAddCard)
+    return (
+      <article className={styles['card']}>
+        <button className={`${styles['card__button--add']}`}>
+          <img src={plusImg} alt='플러스 아이콘' />
+        </button>
+      </article>
+    );
+
+  /* 폰트 확인 후 해당 폰트로 보여줘야 함 */
+  const { sender, profileImageURL, relationship, content, createAt } = cardData;
+
   return (
-    <section className={styles['card']}>
-      <div className={styles['card__main']}>
-        <div className={styles['card__author']}>
-          <img src={defaultImg} className={styles['card__author-image']} alt='프로필 이미지' />
-          <div className={styles['card__author-info']}>
-            <div className={styles['card__author-text']}>
-              From. <div className={styles['card__author-name']}>김동운</div>
+    <article className={styles['card']}>
+      <div className={styles['card__container']}>
+        <div className={styles['card__main']}>
+          <header className={styles['card__author']}>
+            <div className={styles['card__author-content']}>
+              <img
+                src={profileImageURL ? profileImageURL : defaultImg}
+                className={styles['card__author-image']}
+                alt='프로필 이미지'
+              />
+              <div className={styles['card__author-info']}>
+                <div className={styles['card__author-text']}>
+                  From. <div className={styles['card__author-name']}>{sender}</div>
+                </div>
+                <Badge relationType={relationship} />
+              </div>
             </div>
-            <Badge relationType='동료' />
-          </div>
+            <button className={styles['card__button--delete']}>
+              <DeleteIcon />
+            </button>
+          </header>
+          <hr className={styles['card__divider']} />
+          <section className={styles['card__content']}>{content}</section>
         </div>
-        <hr className={styles['card__divider']} />
-        <div className={styles['card__content']}>
-          코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요! 코로나가 또다시
-          기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요! 코로나가 또다시 기승을 부리는
-          요즘이네요. 건강, 체력 모두 조심 또 하세요! 코로나가 또다시 기승을 부리는 요즘이네요.
-          건강, 체력 모두 조심 또 하세요! 코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두
-          조심 또 하세요!
-        </div>
+        <footer className={styles['card__date']}>{createAt}</footer>
       </div>
-      <div className={styles['card__date']}>2023.07.08</div>
-    </section>
+    </article>
   );
-}
+};
 
 export default ItemCard;
