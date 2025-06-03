3a15a3aea226039fb69846c92ea5c15c8cbad015
Sea0Soda <gotjd9704@gmail.com>
2025-06-03 22:40:46 +0900

    style: scss 스타일 변수화 및 리팩토링

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -12,26 +12,30 @@ function Button({
 
   let className = '';
 
+  //  1. 유효한 사이즈 검사 (Outlined에서만 필요)
+  const allowedSizes = ['56', '40', '36', '28', 'small', 'large'];
+  if (!allowedSizes.includes(size)) {
+    throw new Error(` 지원하지 않는 size: ${size}`);
+  }
+
   if (variant === 'secondary') {
     className = isDisabled ? styles['secondary-small-disabled'] : styles['secondary-small'];
   } else if (variant === 'outlined') {
+    // 아이콘 전용 (예외 케이스)
     if (iconOnly && size === '36') {
       className = styles['outlined-icon-36'];
-    } else if (size === '40') {
-      className = isDisabled ? styles['outlined-40-disabled'] : styles['outlined-40'];
-    } else if (size === '36') {
-      className = isDisabled ? styles['outlined-36-disabled'] : styles['outlined-36'];
-    } else if (size === '28') {
-      className = isDisabled ? styles['outlined-28-disabled'] : styles['outlined-28'];
     } else {
-      className = isDisabled ? styles['outlined-56-disabled'] : styles['outlined-56'];
+      const key = `outlined-${size}${isDisabled ? '-disabled' : ''}`;
+      className = styles[key];
     }
   } else {
+    // primary 버튼
     className =
       size === 'small'
-        ? styles[`${state}-small`] // 예: enabled-small
-        : styles[state]; // 예: enabled
+        ? styles[`${state}-small`] // enabled-small 등
+        : styles[state]; // enabled, disabled
   }
+
   return (
     <button className={className} disabled={isDisabled}>
       {children}
