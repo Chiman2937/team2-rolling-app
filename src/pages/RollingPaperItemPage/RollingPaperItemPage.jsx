11f6fbbbe3e66442b7103236ce218cc8d7e52ffa
summerdev96 <developingsummer96@gmail.com>
2025-06-03 18:46:22 +0900

    feat: 카드 컴포넌트 작업, 롤링 상세페이지 작업

--- src/pages/RollingPaperItemPage/RollingPaperItemPage.jsx
+++ src/pages/RollingPaperItemPage/RollingPaperItemPage.jsx
@@ -1,7 +1,64 @@
 import styles from '@/pages/RollingPaperItemPage/RollingPaperItemPage.module.scss';
 import ItemCard from '../../components/ItemCard';
-import Dropdown from '../../components/Dropdown';
-import Textfield from '../../components/Textfield';
+
+/* 테스트 데이터 */
+const itemList = [
+  {
+    id: 1,
+    recipientId: 1,
+    sender: '배수민',
+    profileImageURL: '',
+    relationship: '동료',
+    content:
+      '내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.',
+    font: 'Pretendard',
+    createAt: '2025-06-03T10:15:30Z',
+  },
+  {
+    id: 2,
+    recipientId: 2,
+    sender: '김치영',
+    profileImageURL: '',
+    relationship: '동료',
+    content:
+      '내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.',
+    font: 'Pretendard',
+    createAt: '2025-06-03T10:15:30Z',
+  },
+  {
+    id: 3,
+    recipientId: 3,
+    sender: '이수연',
+    profileImageURL: '',
+    relationship: '동료',
+    content:
+      '내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.',
+    font: 'Pretendard',
+    createAt: '2025-06-03T10:15:30Z',
+  },
+  {
+    id: 4,
+    recipientId: 4,
+    sender: '정해성',
+    profileImageURL: '',
+    relationship: '동료',
+    content:
+      '내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.',
+    font: 'Pretendard',
+    createAt: '2025-06-03T10:15:30Z',
+  },
+  {
+    id: 5,
+    recipientId: 5,
+    sender: '염휘건',
+    profileImageURL: '',
+    relationship: '동료',
+    content:
+      '내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다. 내용입니다.',
+    font: 'Pretendard',
+    createAt: '2025-06-03T10:15:30Z',
+  },
+];
 
 function RollingPaperItemPage({ bgColor = 'skyblue', bgImage }) {
   const containerStyle = {
@@ -14,11 +71,17 @@ function RollingPaperItemPage({ bgColor = 'skyblue', bgImage }) {
   return (
     <>
       {/* 헤더 영역 */}
-      <div style={containerStyle} className={styles['list']}>
-        <div className={styles['list__card']}>
-          <ItemCard />
+      <section style={containerStyle} className={styles['list']}>
+        <div className={styles['list__container']}>
+          <button className={styles['list__button']}>삭제하기</button>
+          <div className={styles['list__card']}>
+            <ItemCard isAddCard />
+            {itemList.map((item) => (
+              <ItemCard key={item.id} cardData={item} />
+            ))}
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,5 @@
 import { lazy } from 'react';
+import RollingPaperItemPage from '../pages/RollingPaperItemPage/RollingPaperItemPage';
 // 각 페이지의 주소와 컴포넌트를 매핑하는 라우트 설정 파일
 // lazy를 사용하면, 컴포넌트가 필요할 때만 불러와서 초기 로딩 속도를 개선할 수 있습니다.
 
@@ -12,4 +13,5 @@ export const routes = [
   //ex2: { path: '/list', element: ListPage },
   { path: '/', element: TestPage },
   // 추가 라우트..
+  { path: 'post/:id', element: RollingPaperItemPage }, // 상세 목록 페이지
 ];
