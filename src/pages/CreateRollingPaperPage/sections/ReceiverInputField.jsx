8c4d1904457eadd4926f574f9e5f854735395207
Kim Chiyoung <cyga2933@gmail.com>
2025-06-04 21:16:05 +0900

    fix: TextField에 props로 전달하는 state를 isValid로 수정

--- src/pages/CreateRollingPaperPage/sections/ReceiverInputField.jsx
+++ src/pages/CreateRollingPaperPage/sections/ReceiverInputField.jsx
@@ -3,11 +3,12 @@ import styles from './ReceiverInputField.module.scss';
 import Textfield from '../../../components/Textfield';
 
 const ReceiverInputField = ({ receiver, setReceiver }) => {
-  const [isEditingReceiver, setIsEditingReceiver] = useState(false);
+  const [isValid, setIsValid] = useState(null);
 
   // input change event
   const handleInputChange = (value) => {
-    setIsEditingReceiver(true);
+    if (!value) setIsValid(false);
+    else setIsValid(true);
     setReceiver(value);
   };
 
@@ -18,7 +19,7 @@ const ReceiverInputField = ({ receiver, setReceiver }) => {
         value={receiver}
         placeholder='받는 사람 이름을 입력해주세요'
         onChange={handleInputChange}
-        isError={isEditingReceiver ? receiver === '' : null}
+        isValid={isValid}
         message='이름이 입력되지 않았습니다'
       />
     </article>
