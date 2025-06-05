10bde5df994c6c3f72d89c82938cda1f926c0ac7
summerdev96 <developingsummer96@gmail.com>
2025-06-05 09:38:15 +0900

    fix: useMessageItemsList useEffect, loadMore 함수 로직 간소화

--- src/hooks/useMessageItemsList.jsx
+++ src/hooks/useMessageItemsList.jsx
@@ -3,42 +3,38 @@ import { useApi } from '@/hooks/useApi.jsx';
 import { listRecipientMessages } from '@/apis/recipientMessageApi';
 
 export const useMessageItemsList = (id) => {
-  const [itemList, setItemList] = useState([]);
-  const [hasNext, setHasNext] = useState(null);
-  const [offset, setOffset] = useState(0);
-
   const {
     data: getMessageListData,
     loading,
     refetch: getMessageListRefetch,
   } = useApi(listRecipientMessages, { recipientId: id, limit: 8, offset: 0 }, { immediate: true });
 
+  const [itemList, setItemList] = useState([]);
+  const hasNext = !!getMessageListData?.next;
+  const [offset, setOffset] = useState(0);
+
+  /* API 실행 후 데이터 세팅 */
+  useEffect(() => {
+    if (!getMessageListData) return;
+    const { results, previous } = getMessageListData;
+    setItemList((prevList) => (offset === 0 || !previous ? results : [...prevList, ...results]));
+  }, [getMessageListData, offset]);
+
+  /* 스크롤 시 데이터 다시 불러옴  */
   const loadMore = useCallback(() => {
     if (loading || !hasNext) return;
-    setOffset((prevOffset) => (prevOffset === 0 ? prevOffset + 8 : prevOffset + 6));
-  }, [loading, hasNext]);
 
+    const newOffset = offset === 0 ? offset + 8 : offset + 6;
+    getMessageListRefetch({ recipientId: id, limit: 6, offset: newOffset });
+    setOffset(newOffset);
+  }, [loading, hasNext, offset, id, getMessageListRefetch]);
+
+  /* 삭제 후 데이터 초기 상태로 불러옴 */
   const initializeList = () => {
     if (loading) return;
     setOffset(0);
     getMessageListRefetch({ recipientId: id, limit: 8, offset: 0 });
   };
-
-  {
-    /* 스크롤 시 offset 변경되어 API 재실행 */
-  }
-  useEffect(() => {
-    if (offset === 0) return;
-    getMessageListRefetch({ recipientId: id, limit: 6, offset });
-  }, [offset, id, getMessageListRefetch]);
-
-  /* API 실행 후 데이터 세팅 */
-  useEffect(() => {
-    if (!getMessageListData) return;
-    const { results, next, previous } = getMessageListData;
-    setItemList((prevList) => (offset === 0 || !previous ? results : [...prevList, ...results]));
-    setHasNext(!!next);
-  }, [getMessageListData, offset]);
 
   return { itemList, hasNext, loadMore, initializeList, loading };
 };
