6ffa210c4cff44c11af20c8cd124d4c0c3e0c4a1
Kim Chiyoung <cyga2933@gmail.com>
2025-06-09 00:26:46 +0900

    refactor: LoadingOverlay inline style css파일로 이동

--- src/components/LoadingOverlay.jsx
+++ src/components/LoadingOverlay.jsx
@@ -3,22 +3,7 @@ import styles from './LoadingOverlay.module.scss';
 
 const LoadingOverlay = ({ description = '' }) => {
   return (
-    <section
-      style={{
-        width: '100%',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        opacity: '0.9',
-        backgroundColor: 'var(--color-white)',
-        zIndex: '99',
-      }}
-    >
+    <section className={styles['loading-overlay']}>
       <LoadingSpinner.dotCircle dotCount={12} dotSize={8} distanceFromCenter={40} />
       <span className={styles['loading-overlay__title']}>잠시만 기다려주세요</span>
       <span className={styles['loading-overlay__description']}>{description}</span>
