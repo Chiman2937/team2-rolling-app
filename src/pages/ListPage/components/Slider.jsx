219d861ee05d0e115d0aaf499ef436cff4aeafc2
summerdev96 <developingsummer96@gmail.com>
2025-06-09 20:30:37 +0900

    feat: 무한스크롤 가로로 스크롤 될 경우 추가

--- src/pages/ListPage/components/Slider.jsx
+++ src/pages/ListPage/components/Slider.jsx
@@ -1,14 +1,23 @@
+import { useRef } from 'react';
 import styles from './Slider.module.scss';
 import ItemCard from './ItemCard';
 import ArrowButton from '../../../components/Button/ArrowButton';
 import HorizontalScrollContainer from '../../../components/HorizontalScrollContainer/HorizontalScrollContainer';
 import { useSliderPaging } from '@/hooks/useSliderPaging';
+import InfinityScrollWrapper from '@/components/InfinityScrollWrapper/InfinityScrollWrapper';
 
 const CARD_WIDTH = 275;
 const GAP = 16;
 const PAGE_SIZE = 4;
 
 const Slider = ({ cards }) => {
+  /* 무한 스크롤에서 스크롤 감지를 위함 */
+  const scrollObserverRef = useRef(null);
+  /* Api 요청 데이터에서 next값이 있는지 확인, true 일때만 데이터를 불러옴 */
+  const hasNext = true;
+  /* 추가 데이터 로드 */
+  const loadMore = () => {};
+
   const { wrapperRef, isDesktop, pageIndex, canPrev, canNext, goPrev, goNext } = useSliderPaging({
     totalItems: cards.length,
     pageSize: PAGE_SIZE,
@@ -31,12 +40,14 @@ const Slider = ({ cards }) => {
       )}
 
       <div ref={wrapperRef} className={styles['slider__container']}>
-        <HorizontalScrollContainer>
-          <div className={styles['slider__track']}>
-            {visibleCards.map((c) => (
-              <ItemCard key={c.id} data={c} />
-            ))}
-          </div>
+        <HorizontalScrollContainer ref={scrollObserverRef} hideScroll={false}>
+          <InfinityScrollWrapper hasNext={hasNext} callback={loadMore} isHorizontal>
+            <div className={styles['slider__track']}>
+              {visibleCards.map((c) => (
+                <ItemCard key={c.id} data={c} />
+              ))}
+            </div>
+          </InfinityScrollWrapper>
         </HorizontalScrollContainer>
       </div>
 
