3353b43bd14a99adc15cef11909019531307a5c1
summerdev96 <developingsummer96@gmail.com>
2025-06-09 17:56:02 +0900

    fix: 카드, 모달 내 프로필 부분 한줄이상인 경우 말줄임 적용

--- src/pages/RollingPaperItemPage/components/ListButtonGroup.jsx
+++ src/pages/RollingPaperItemPage/components/ListButtonGroup.jsx
@@ -4,19 +4,21 @@ import { useDeviceType } from '@/hooks/useDeviceType';
 
 const ListButtonGroup = ({ showDelete, onClickEdit, onClickPrev, onClickGoList }) => {
   const deviceType = useDeviceType();
+  const buttonSize = deviceType !== 'desktop' ? 'stretch' : 'small';
+
   return (
     <>
       <div className={styles['list__button-group']}>
-        <Button size={deviceType === 'desktop' ? 'small' : 'stretch'} onClick={onClickGoList}>
+        <Button size={buttonSize} onClick={onClickGoList}>
           목록으로
         </Button>
         {!showDelete && (
-          <Button size={deviceType === 'desktop' ? 'small' : 'stretch'} onClick={onClickEdit}>
+          <Button size={buttonSize} onClick={onClickEdit}>
             편집하기
           </Button>
         )}
         {showDelete && (
-          <Button size={deviceType === 'desktop' ? 'small' : 'stretch'} onClick={onClickPrev}>
+          <Button size={buttonSize} onClick={onClickPrev}>
             돌아가기
           </Button>
         )}
