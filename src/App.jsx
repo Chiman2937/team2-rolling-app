3869c775ee40e8fe21dad20eefa73902a57f0861
Kim Chiyoung <cyga2933@gmail.com>
2025-06-09 17:13:57 +0900

    fix: healmet 삭제 후 meta 태그 html로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
 import Routes from '@/routes/AppRouter';
 import Style from '@/assets/styles/App.module.scss';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const App = () => {
   return (
-    <HelmetProvider>
-      <Helmet>
-        <title>Rolling</title>
-        <meta name='description' content='Rolling : 롤링페이퍼 커뮤니티 플랫폼' />
-        <meta property='og:title' content='Rolling' />
-        <meta property='og:description' content='친구나 동료들과 따뜻한 메시지를 주고받아 보세요' />
-        <meta
-          property='og:image'
-          content={`${window.location.origin}/images/image_opengraph.png`}
-        />
-        <meta property='og:image:width' content='1200' />
-        <meta property='og:image:height' content='630' />
-        <meta property='og:url' content={window.location.href} />
-        <meta property='og:type' content='website' />
-      </Helmet>
-      <div className={Style['app']}>
-        <main>
-          {/* 라우터에 의해 페이지가 랜더링 */}
-          <Routes />
-        </main>
-      </div>
-    </HelmetProvider>
+    <div className={Style['app']}>
+      <main>
+        {/* 라우터에 의해 페이지가 랜더링 */}
+        <Routes />
+      </main>
+    </div>
   );
 };
 
