6fde8c10ec4beb7fa53e4e3479ca0fc3fd33ae25
Kim Chiyoung <cyga2933@gmail.com>
2025-06-10 01:16:50 +0900

    refactor: 각 ..postData.js 파일 내부 필요없는 함수 제거

--- src/utils/getBackgroundStylesFromPostData.js
+++ src/utils/getBackgroundStylesFromPostData.js
@@ -1,3 +1,5 @@
+import { getColorFromCloudinaryImageUrl } from './getColorFromCloudinaryImageUrl';
+
 export const getBackgroundStylesFromPostData = ({ backgroundColor, backgroundImageURL }) => {
   let backgroundStyle = {
     backgroundColor: 'none',
@@ -14,7 +16,7 @@ export const getBackgroundStylesFromPostData = ({ backgroundColor, backgroundIma
     const parts = urlObj.pathname.split('/'); // ['','dxho7f5dm','image','upload','v1749409044','colors','6a6a6a.png']
     const folderCandidate = parts[5]; // public_id 시작 부분 (index 5)
     if (folderCandidate === 'colors') {
-      const imageHexColor = getColorFromCloudinaryImage(backgroundImageURL);
+      const imageHexColor = getColorFromCloudinaryImageUrl(backgroundImageURL);
       backgroundStyle.backgroundColor = imageHexColor;
     } else if (folderCandidate === 'images') {
       backgroundStyle.backgroundImage = `url(${backgroundImageURL})`;
@@ -24,21 +26,4 @@ export const getBackgroundStylesFromPostData = ({ backgroundColor, backgroundIma
   }
 
   return backgroundStyle;
-};
-
-//cloudinary 파일명에서 색상을 읽어옵니다.
-//ex. ...colors/131313.svg는 #131313 입니다.
-const getColorFromCloudinaryImage = (url) => {
-  if (!url) return null;
-  const urlObj = new URL(url);
-  const parts = urlObj.pathname.split('/'); // ['','dxho7f5dm','image','upload','v1749409044','colors','6a6a6a.png']
-  const isHexColor = (str) => /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/.test(str);
-  const fileName = parts[6];
-  if (!fileName) return null;
-  const hexString = '#' + fileName.substring(0, 6); // public_id 시작 부분 (index 5)
-  if (isHexColor(hexString)) {
-    return hexString;
-  } else {
-    return null; // 기본값
-  }
 };
--- src/utils/getContentStylesFromPostData.js
+++ src/utils/getContentStylesFromPostData.js
@@ -1,3 +1,4 @@
+import { getColorFromCloudinaryImageUrl } from './getColorFromCloudinaryImageUrl';
 import { getIsColorDark } from './getIsColorDark';
 
 export const getContentStylesFromPostData = (backgroundImageURL) => {
@@ -19,7 +20,7 @@ export const getContentStylesFromPostData = (backgroundImageURL) => {
     const parts = urlObj.pathname.split('/'); // ['','dxho7f5dm','image','upload','v1749409044','colors','6a6a6a.png']
     const folderCandidate = parts[5]; // public_id 시작 부분 (index 5)
     if (folderCandidate === 'colors') {
-      const imageHexColor = getColorFromCloudinaryImage(backgroundImageURL);
+      const imageHexColor = getColorFromCloudinaryImageUrl(backgroundImageURL);
       const isDark = getIsColorDark(imageHexColor);
       if (isDark) {
         return contentDarkColorStyle;
@@ -29,22 +30,5 @@ export const getContentStylesFromPostData = (backgroundImageURL) => {
     } else {
       return contentImageStyle;
     }
-  }
-};
-
-//cloudinary 파일명에서 색상을 읽어옵니다.
-//ex. ...colors/131313.svg는 #131313 입니다.
-const getColorFromCloudinaryImage = (url) => {
-  if (!url) return null;
-  const urlObj = new URL(url);
-  const parts = urlObj.pathname.split('/'); // ['','dxho7f5dm','image','upload','v1749409044','colors','6a6a6a.png']
-  const isHexColor = (str) => /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/.test(str);
-  const fileName = parts[6];
-  if (!fileName) return null;
-  const hexString = '#' + fileName.substring(0, 6); // public_id 시작 부분 (index 5)
-  if (isHexColor(hexString)) {
-    return hexString;
-  } else {
-    return null; // 기본값
   }
 };
