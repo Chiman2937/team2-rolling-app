ca9f5a3a93df5deee651858b56fd353a3c74fe71
Sea0Soda <gotjd9704@gmail.com>
2025-06-09 12:18:29 +0900

    refactor: Button 컴포넌트 시스템 개선

--- src/components/Button/IconButton.jsx
+++ src/components/Button/IconButton.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import styles from './IconButton.module.scss';
 
-function IconButton({ icon, enabled = true, onClick }) {
+function IconButton({
+  icon, //  이미지 경로만 받음
+  enabled = true,
+  onClick,
+  className, //
+  ...rest //  나머지 props 지원
+}) {
   const isDisabled = !enabled;
 
   return (
     <button
-      className={isDisabled ? styles.disabled : styles.base}
+      className={`${isDisabled ? styles.disabled : styles.base} ${className || ''}`.trim()}
       disabled={isDisabled}
       onClick={onClick}
+      {...rest}
     >
-      {icon}
+      {/*  자동으로 24×24 아이콘 생성 */}
+      <img src={icon} alt='' width={24} height={24} />
     </button>
   );
 }
