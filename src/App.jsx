2c3debbcc9207278f05150350ef8ceb10424f935
Kim Chiyoung <cyga2933@gmail.com>
2025-06-09 17:09:25 +0900

    fix: App.jsx에 meta data 조건부 렌더링 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
 import Routes from '@/routes/AppRouter';
 import Style from '@/assets/styles/App.module.scss';
-import { useLocation } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const App = () => {
-  const location = useLocation();
-
   return (
     <HelmetProvider>
       <Helmet>
         <title>Rolling</title>
         <meta name='description' content='Rolling : 롤링페이퍼 커뮤니티 플랫폼' />
+        <meta property='og:title' content='Rolling' />
+        <meta property='og:description' content='친구나 동료들과 따뜻한 메시지를 주고받아 보세요' />
+        <meta
+          property='og:image'
+          content={`${window.location.origin}/images/image_opengraph.png`}
+        />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
+        <meta property='og:url' content={window.location.href} />
+        <meta property='og:type' content='website' />
       </Helmet>
-      {location.pathname === '/' && (
-        <Helmet>
-          <meta property='og:title' content='Rolling' />
-          <meta
-            property='og:description'
-            content='친구나 동료들과 따뜻한 메시지를 주고받아 보세요'
-          />
-          <meta
-            property='og:image'
-            content={`${window.location.origin}/images/image_opengraph.png`}
-          />
-          <meta property='og:image:width' content='1200' />
-          <meta property='og:image:height' content='630' />
-          <meta property='og:url' content={window.location.href} />
-          <meta property='og:type' content='website' />
-        </Helmet>
-      )}
-
       <div className={Style['app']}>
         <main>
           {/* 라우터에 의해 페이지가 랜더링 */}
