1caba4c57e3dae6c1921b5d41e319a5ca7913680
Hwigeon <bb7788@naver.com>
2025-06-13 01:55:35 +0900

    feat: notFoundPage 디자인

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,26 +1,25 @@
 // src/pages/NotFoundPage.jsx
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-
+import styles from './NotFoundPage.module.scss';
+import Button from '@/components/Button/Button';
 export default function NotFoundPage() {
   const navigate = useNavigate();
   //현재 경로 불러오기
   const currentPath = useLocation().pathname;
   return (
     // @todo: BEM 및 scss 모듈 적용
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: '80px 20px',
-        gap: '20px',
-      }}
-    >
-      <h1>404</h1>
-      <h1>'{currentPath}'경로를 찾을 수 없습니다.</h1>
-      <p>요청하신 페이지가 존재하지 않습니다.</p>
-      <button onClick={() => navigate('/')}>홈으로 돌아가기</button>
+    <div className={styles['not-found-page']}>
+      <h1 className={styles['not-found-page__title']}>404</h1>
+      <h3 className={styles['not-found-page__subtitle']}>
+        <span className={styles['not-found-page__path']}> '{currentPath}'</span> 경로를 찾을 수
+        없습니다.
+      </h3>
+      <div className={styles['not-found-page__message']}>
+        <p>요청하신 페이지가 존재하지 않습니다.</p>
+        <p>입력하신 경로가 올바른지 확인해 주세요.</p>
+      </div>
+      <Button onClick={() => navigate('/')}>홈으로 돌아가기</Button>
     </div>
   );
 }
