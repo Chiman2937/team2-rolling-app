9993f5bc15d52b89ffeb48c54e1cc1d90abfe3cd
summerdev96 <developingsummer96@gmail.com>
2025-06-12 15:33:52 +0900

    feat: 롤링 상세페이지 삭제 아이콘 오류 수정 및 버튼 아이콘 추가

--- src/pages/RollingPaperItemPage/components/ListButtonGroup.jsx
+++ src/pages/RollingPaperItemPage/components/ListButtonGroup.jsx
@@ -1,6 +1,9 @@
 import styles from './ListButtonGroup.module.scss';
 import Button from '@/components/Button/Button';
 import { useDeviceType } from '@/hooks/useDeviceType';
+import BackIcon from './BackIcon';
+import EditIcon from './EditIcon';
+import CarouselIcon from './CarouselIcon';
 
 const ListButtonGroup = ({ showDelete, onClickEdit, onClickPrev, onClickGoList }) => {
   const deviceType = useDeviceType();
@@ -9,17 +12,24 @@ const ListButtonGroup = ({ showDelete, onClickEdit, onClickPrev, onClickGoList }
   return (
     <>
       <div className={styles['list__button-group']}>
-        <Button size={buttonSize} variant={'secondary'} onClick={onClickGoList}>
+        <Button
+          size={buttonSize}
+          variant={'secondary'}
+          onClick={onClickGoList}
+          className={styles['list__button']}
+        >
+          <CarouselIcon />
           목록으로
         </Button>
         {!showDelete && (
-          <Button size={buttonSize} onClick={onClickEdit}>
+          <Button size={buttonSize} onClick={onClickEdit} className={styles['list__button']}>
+            <EditIcon />
             편집하기
           </Button>
         )}
         {showDelete && (
-          <Button size={buttonSize} onClick={onClickPrev}>
-            돌아가기
+          <Button size={buttonSize} onClick={onClickPrev} className={styles['list__button']}>
+            <BackIcon /> 돌아가기
           </Button>
         )}
       </div>
